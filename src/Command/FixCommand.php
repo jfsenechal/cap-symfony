89d9f39247a3e9e0ced9ed7526dffd2b15f0257f
jfsenechal <jfsenechal@gmail.com>
2023-04-26 11:40:18 +0200

    add fix command

---
+++ src/Command/FixCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cap\Commercio\Command;
+
+use Cap\Commercio\Repository\PaymentOrderRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'cap:fix',
+    description: 'Add a short description for your command',
+)]
+class FixCommand extends Command
+{
+    public function __construct(private PaymentOrderRepository $paymentOrderRepository)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        foreach ($this->paymentOrderRepository->findAll() as $order) {
+            if ($order->getPdfPath() == null) {
+                $io->error('Vide'.$order->getId().$order->getOrderCommercant()->getCompanyName());
+                continue;
+            }
+            $io->writeln($order->getPdfPath());
+        }
+
+        return Command::SUCCESS;
+    }
+}
