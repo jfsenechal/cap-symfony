1b5ab1785c3830f5d1e990c7fb23e2a51078a1a5
jfsenechal <jfsenechal@gmail.com>
2023-04-27 22:29:48 +0200

    add blog post

--- src/Form/BlogPostType.php
+++ src/Form/BlogPostType.php
@@ -4,6 +4,8 @@ namespace Cap\Commercio\Form;
 
 use Cap\Commercio\Entity\BlogPost;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -26,7 +28,21 @@ class BlogPostType extends AbstractType
                 'label' => 'Texte',
                 'required' => true,
                 'attr'=>['rows'=>5]
-            ]);
+            ])
+            ->add('archived', CheckboxType::class, [
+                'label' => 'Archivé',
+                'required' => false,
+            ])
+            ->add('publishDate', DateType::class, [
+                'label' => 'Publié le',
+                'widget'=>'single_text',
+                'required' => true,
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Intitule',
+                'required' => true,
+            ])
+      ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
