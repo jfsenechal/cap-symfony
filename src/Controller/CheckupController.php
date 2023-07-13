c10020c4de18d37939758c687282bef591245096
jfsenechal <jfsenechal@gmail.com>
2023-07-13 16:35:48 +0200

    add checkup pdf path

--- src/Controller/CheckupController.php
+++ src/Controller/CheckupController.php
@@ -2,6 +2,8 @@
 
 namespace Cap\Commercio\Controller;
 
+use Cap\Commercio\Entity\PaymentBill;
+use Cap\Commercio\Entity\PaymentOrder;
 use Cap\Commercio\Repository\CommercioCommercantRepository;
 use Cap\Commercio\Repository\PaymentBillRepository;
 use Cap\Commercio\Repository\PaymentOrderRepository;
@@ -14,6 +16,8 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_CAP')]
 class CheckupController extends AbstractController
 {
+    private string $path = '/var/www/sites/commercio/';
+
     public function __construct(
         private PaymentOrderRepository $paymentOrderRepository,
         private PaymentBillRepository $paymentBillRepository,
@@ -86,6 +90,47 @@ class CheckupController extends AbstractController
                 'today' => $today,
             ]
         );
+    }
+
+    #[Route(path: '/missingpath', name: 'cap_checkup_missing_path', methods: ['GET'])]
+    public function missingPaht(): Response
+    {
+        $ordersMissing = $billsMissing = $ordersPath = $billsPath = [];
+        foreach ($this->paymentOrderRepository->findAll() as $order) {
+            if ($order->getPdfPath() == null) {
+                $ordersMissing[] = $order;
+
+            }
+            if (!is_readable($this->getAbsolutePathPdf($order))) {
+                $ordersPath[] = $order;
+            }
+        }
+
+        foreach ($this->paymentBillRepository->findAll() as $bill) {
+            if ($bill->getPdfPath() == null) {
+                $billsMissing[] = $bill;
+            }
+            if (!is_readable($this->getAbsolutePathPdf($bill))) {
+                $billsPath = $bill;
+            }
+        }
+
+        return $this->render(
+            '@CapCommercio/checkup/missing.html.twig',
+            [
+                'ordersMissing' => $ordersMissing,
+                'billsMissing' => $billsMissing,
+                'billsPath' => $billsPath,
+                'ordersPath' => $ordersPath,
+            ]
+        );
+    }
+
+    private function getAbsolutePathPdf(PaymentBill|PaymentOrder $object)
+    {
+        list($name) = explode('?', $object->getPdfPath());
+
+        return $this->path.$name;
     }
 
 }
