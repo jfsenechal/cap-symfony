fc61703784eeeb30181e5ee906627e771eb04f0c
jfsenechal <jfsenechal@gmail.com>
2023-08-21 11:18:38 +0200

    test ai

--- src/Service/OpenAiCap.php
+++ src/Service/OpenAiCap.php
@@ -5,6 +5,7 @@ namespace Cap\Commercio\Service;
 use OpenAI;
 use OpenAI\Responses\Completions\CreateResponse;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use OpenAI\Responses\Models\ListResponse;
 
 class OpenAiCap
 {
@@ -23,5 +24,13 @@ class OpenAiCap
             'model' => 'text-davinci-003',
             'prompt' => $prompt,
         ]);
+    }
+
+    public function modelList(): ListResponse
+    {
+        $yourApiKey = getenv($this->openAiKey);
+        $client = OpenAi::client($yourApiKey);
+
+        return $client->models()->list();
     }
 }
