2226ac06b10e5d95a47028195a26eaa93b5537b1
jfsenechal <jfsenechal@gmail.com>
2024-01-08 17:12:37 +0100

    attribute route to annotation

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,7 +6,7 @@ use Cap\Commercio\Repository\ContactParamsRepository;
 use Cap\Commercio\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route(path: '/contact')]
